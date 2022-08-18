99b8e60baa6dda96ca736a4a69e5c423c74eaf13
luuhoa0801 <hoathuytien12369@gmail.com>
2022-08-18 19:50:01 +0700

    188

--- ss6_angular_form/exercise/login-angular/src/app/login/login.component.ts
+++ ss6_angular_form/exercise/login-angular/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  public loginForm: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    })
+  }
+  getPassword() {
+    return this.loginForm.get('password');
+  }
+  onSubmit() {
+    console.log(this.loginForm.value);
   }
 
 }
